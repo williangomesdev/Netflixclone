2c2a94ab7bd320cacf6234f22b48a9f656e23be1
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-02-22 17:49:09 -0300

    list component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import "../styles/App.css";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(() => {
     const loadAll = async () => {
       //pegando lista total
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     };
 
     loadAll();
   }, []);
-  return <div>Olá mundo!!!</div>;
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 };
