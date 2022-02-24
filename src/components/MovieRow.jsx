3ea7620268b042066e87a9d776f23dbb5007c261
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-02-24 15:21:43 -0300

    arrows functions added

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import ArrowForwardIos from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIos from "@mui/icons-material/ArrowBackIos";
 
 export default ({ title, items }) => {
+  //Listar os cartazes para esquerda ou direita
+  const [scrollX, setscrollX] = useState(-400);
+
+  //botão esquerdo, a cada clique o movie-list ira se mover pela metade da tela do dispositivo
+  const handleLeftArrow = () => {
+    //(1400 + 1/2 tela do dispositivo)
+    let x = scrollX + Math.round(window.innerWidth / 2);
+
+    //Quando x > 0 = o mesmo vai para no ultimo cartaz
+    if (x > 0) {
+      x = 0;
+    }
+
+    setscrollX(x);
+  };
+
+  //botão direito, a cada clique o movie-list ira se mover pela metade da tela do dispositivo
+  const handleRightArrow = () => {
+    //(-400 - 1/2 tela do dispositivo)
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    //pegar a quantidade de cartazes na tela
+    let listW = items.results.length * 150;
+    //diferença do tamanho da tela e quantidade de cartazes da tela do dispositivo
+    let difference = window.innerWidth - listW;
+    //se difference for maior que o valor de x, o mesmo fica com o valor de difference - o valor da largura das duas arrow (30px cada)
+    if (difference > x) {
+      x = difference - 80;
+    }
+    setscrollX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow--left">
+      <div className="movieRow--left" onClick={handleLeftArrow}>
         <ArrowBackIos style={{ fontSize: 50 }} />
       </div>
-      <div className="movieRow--right">
+      <div className="movieRow--right" onClick={handleRightArrow}>
         <ArrowForwardIos style={{ fontSize: 50 }} />
       </div>
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrollX, width: items.results.length * 150 }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow--item">
